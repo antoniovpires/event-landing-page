ca11270a16cf26854547fd44afbe4a737398cf1e
Antônio Pires <antoniovpiresnt@gmail.com>
2023-07-21 18:06:57 -0300

    changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+AOS.init();
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 1000
+
+
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
@@ -5,6 +11,23 @@ window.addEventListener('scroll', () => {
       header.classList.add('scrolled');
     } else {
       header.classList.remove('scrolled');
+    }
+
+    if (window.scrollY > 750) {
+      valueDisplays.forEach((valueDisplay) => {
+        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+        if (parseInt(valueDisplay.textContent) > 0) return false
+        let startValue = 0;
+      
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(() => {
+          startValue += endValue > 100 ? 10 : 1;
+          valueDisplay.textContent = `${startValue}+`;
+          if(startValue ==endValue) {
+            clearInterval(counter);
+          }
+        }, duration)
+      })
     }
   });
 
